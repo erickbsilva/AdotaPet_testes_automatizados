3f65fa3566e340fbb0534b08ed6476031716d57a
erick <erickbarretodasilva@gmail.com>
2026-03-12 20:17:28 -0300

    utilizando um dto e convertendo para json dentro controller de adoção funcionando

--- src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -1,13 +1,17 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.service.AdocaoService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,10 +19,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class AdocaoControllerTest {
 
     @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<SolicitacaoAdocaoDto> jsonDto;
 
     // não vai usar a service de verdade, e sim um mock do serviço
     @MockBean
@@ -60,6 +71,23 @@ class AdocaoControllerTest {
 
         //ASSERT
         Assertions.assertEquals(200, response.getStatus());
+    }
+
+    @Test
+    void deveriaDevolverCodigo200ParaRequsisicaoDeSolicitarAdocaoComDadosValidos() throws Exception {
+        //ARRANGE
+        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(1l, 1l, "Motivo qualquer");
+
+        //ACT
+        MockHttpServletResponse response = mockMvc.perform(
+                post("/adocoes")
+                        .content(jsonDto.write(dto).getJson())
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+        assertEquals(200, response.getStatus());
+        assertEquals("Adoção solicitada com sucesso!", response.getContentAsString());
     }
 
 }
