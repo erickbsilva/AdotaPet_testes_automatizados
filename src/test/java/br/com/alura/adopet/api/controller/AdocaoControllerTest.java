055640211714f4458a07521695fa6ae61ae2fad6
erick <erickbarretodasilva@gmail.com>
2026-03-12 19:58:22 -0300

    teste de unidade do controller de adoção funcionando

---
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,43 @@
+package br.com.alura.adopet.api.controller;
+
+import br.com.alura.adopet.api.service.AdocaoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AdocaoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    // não vai usar a service de verdade, e sim um mock do serviço
+    @MockBean
+    private AdocaoService service;
+
+    @Test
+    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() throws Exception {
+        //ARRANGE
+        String json = "{}";
+
+        //ACT
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+        Assertions.assertEquals(400, response.getStatus());
+    }
+
+}
